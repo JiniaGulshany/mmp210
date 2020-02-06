c4917c53fc4e26a805c9bb0614719ac355af991f
Jinia Gulshany <jiniaguls@gmail.com>
2020-02-06 17:39:00 -0500

    replaced comments

--- self_portrait/sketch.js
+++ self_portrait/sketch.js
@@ -16,25 +16,38 @@ function draw() {
 	stroke ('lime');
 	strokeWeight (2);
 
+//face area 
+	
 	ellipse (200, 180, 200);
 	fill ('red');
-	noStroke();
+
+//right eye
 	ellipse (220, 160, 30, 10);
+
+//left eye
 	ellipse (180, 160, 30, 10);
 	fill ('yellow');
+
+//mouth
 	rectMode (CENTER);
 	rect (200, 220, 80, 10, 5);
+    
+//right ear
+    noStroke ();
     fill ('orange');
 	arc (290, 180, 40, 60, -HALF_PI, HALF_PI);
 	arc (290, 180, 30, 50, -HALF_PI, HALF_PI);
 
-//SHAPES THAT ARE ONLY XY POINTS//
-
+//SHAPES THAT ARE ONLY XY POINTS
+//hair
+    noFill ();
+    noStroke ();
     line(150, 60, 170, 100);
     line(160, 60, 180, 100);
     line(170, 60, 190, 100);
     line(180, 60, 200, 100);
 
+//nose
     fill ('purple');
     triangle (190, 180, 180, 200, 220, 190);
 }

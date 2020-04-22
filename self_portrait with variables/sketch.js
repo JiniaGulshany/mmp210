7bed86cc22b285aa1dfadd17908ae0fb1ed2dd8d
Jinia Gulshany <jiniaguls@gmail.com>
2020-04-22 13:06:58 -0400

    updated variables

--- self_portrait with variables/sketch.js
+++ self_portrait with variables/sketch.js
@@ -18,13 +18,13 @@ function draw() {
   /* x = mouseX; 
 */ 
 	triangle(x, y, x-200, y+160, x+180, y+160);
-	triangle(90, 80, 20, 200, 220, 200);
-	triangle(560, 80, 420, 200, 620, 200);
+	triangle(x-230, y-100, x-300, y+20, x-100, y+20);
+	triangle(x+240, y-100, x+100, y+20, x+300, y+20);
 	fill('orange');
-	circle(115, 165, 70);
-	circle(530, 165, 70);
+	circle(x-205, y-15, 70);
+	circle(x+210, y-15, 70);
     fill('pink'); //tongue
 
-    rect(x-80, y+70, 160, 80, 40);
+    rect(x-80, y+70, x-160, y-100, 40);
 
 }
